084ad6313fae549979c201d7a4e1a5cc31e17fa5
Aadityaa23 <aadityaa32@gmail.com>
2024-01-02 13:56:35 +1100

    finished up with PNV section

--- app/components/PNV.tsx
+++ app/components/PNV.tsx
@@ -5,9 +5,32 @@ export default function PNV() {
 	return (
 		<>
 			<div className="bg-green-950 flex flex-col items-center p-16">
+				{/* Projects */}
 				<h2 className=" text-3xl mb-4">Projects</h2>
 
-				<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-4 gap-y-28 lg:gap-y-16">
+				<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+					<Card
+						title="US 22 Pitch Comp"
+						body="This is the new body from the Projects and Volunteering section"
+						imagePath="team"
+					></Card>
+					<Card
+						title="US 22 Pitch Comp"
+						body="This is the new body from the Projects and Volunteering section"
+						imagePath="team"
+					></Card>
+					<Card
+						title="Capstone Project"
+						body="This is the new body from the Projects and Volunteering section"
+						imagePath="capstone"
+					></Card>
+				</div>
+
+				{/* Volunteering */}
+
+				<h2 className=" text-3xl my-8">Volunteering</h2>
+
+				<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
 					<Card
 						title="US 22 Pitch Comp"
 						body="This is the new body from the Projects and Volunteering section"
