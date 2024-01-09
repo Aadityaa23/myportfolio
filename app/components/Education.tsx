196465cec349d9d2c8d390c2c7a1c18175cc7b58
Aadityaa23 <aadityaa32@gmail.com>
2024-01-09 21:14:02 +1100

    updated educaiton section

--- app/components/Education.tsx
+++ app/components/Education.tsx
@@ -1,29 +1,55 @@
 import Image from "next/image";
-import GradPhoto from "../../public/grad2.jpg";
+import GradPhoto from "../../public/grad.jpg";
 
 export default function Education() {
 	return (
 		<>
 			<div
 				id="education"
-				className="flex flex-col md:flex-row items-center justify-center bg-green-100 py-16 px-8 md:p-0"
+				className="bg-green-100 flex flex-col items-center py-16 px-8"
 			>
-				{/* Photo */}
-				<div className="md:w-1/2">
-					<Image
-						src={GradPhoto}
-						alt="Aadityaa's Photo"
-						className="w-full h-auto rounded-xl md:rounded-sm shadow-md"
-					/>
-				</div>
+				<h2 className="text-black text-3xl mb-4">Education</h2>
 
-				{/* Description */}
-				<div className="md:w-1/2 pt-8 md:pt-0 px-2 md:px-8">
-					<h2 className="text-black text-3xl font-bold mb-4">Education</h2>
-					<p className="text-gray-700">
-						Your brief description goes here. Tell visitors about yourself, your
-						skills, and what you're passionate about.
-					</p>
+				{/* Section 1 */}
+				<div className="flex flex-col md:flex-row items-center justify-center py-8">
+					{/* Photo */}
+					<div className="md:w-1/2 px-2">
+						<Image
+							src={GradPhoto}
+							alt="Aadityaa's photo at graduation"
+							className="w-full h-auto rounded-xl md:rounded-sm shadow-md"
+						/>
+					</div>
+
+					{/* Description */}
+					<div className="md:w-1/2 md:px-8 px-2 pt-8 md:pt-4">
+						<h3 className="text-black text-3xl font-bold mb-1">
+							The University of Melbourne
+						</h3>
+						<h5 className="text-gray-900 text-2xl mb-1">
+							Bachelor of Science (Computing and Software Systems)
+						</h5>
+						<p className="text-gray-800 pl-2">
+							Achieved Second Class Honours, and was rewarded the Leaders in
+							Communities Award, fro outstanding leadership and contribution to
+							the community.
+						</p>
+						<h3 className="text-black text-3xl font-bold mb-1 mt-3">
+							Foundations for the Future Charter Academy
+						</h3>
+						<h5 className="text-gray-900 text-2xl mb-1">High School Diploma</h5>
+						<p className="text-gray-800 pl-2 pb-4">
+							Finished with an overall average of 88. Awarded the Rutherford
+							Scholarship for outstanding academic performance, and the Farlye
+							Mowat Scholarship for exceptional writing and leadership skills.
+						</p>
+						<button
+							type="button"
+							className="bg-green-900 rounded-full pt-2 px-4 text-green-100 hover:bg-green-800 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white shadow-md hover:shadow-lg focus:shadow-lg "
+						>
+							Learn More
+						</button>
+					</div>
 				</div>
 			</div>
 		</>
--- app/components/Hobbies.tsx
+++ app/components/Hobbies.tsx
@@ -11,58 +11,63 @@ export default function Hobbies() {
 				<h2 className=" text-3xl mb-4">Hobbies and Passions</h2>
 
 				{/* Section 1 */}
-				<div className="flex flex-col md:flex-row items-center justify-center my-8">
+				<div className="flex flex-col md:flex-row items-center justify-center my-8 md:px-8">
 					{/* Logo */}
-					<div className="md:w-2/3">
+					<div className="md:w-2/3 md:p-2">
 						<Image
 							src={DEN}
-							alt="Aadityaa's Photo"
+							alt="A group of people wearing costumes are standing on stage"
 							className="w-full h-auto rounded-lg shadow-md"
 						/>
 					</div>
-					<div className="md:w-1/3 py-8 px-2 md:px-8">
-						<h2 className="text-white text-3xl font-bold mb-4">Intro</h2>
+					<div className="md:w-1/3 pt-8 md:pt-0 px-2">
+						<h3 className="text-white text-3xl font-bold mb-4">
+							Theatre and Comedy
+						</h3>
 						<p className="text-gray-200">
-							Your brief description goes here. Tell visitors about yourself,
-							your skills, and what you're passionate about.
+							Do Everything Now Before The World Ends: An Absurdist play that
+							explored the interconnection between the end of the world due to
+							global warming, and what it truly means to be alive.
 						</p>
 					</div>
 				</div>
 
 				{/* Section 2 */}
-				<div className="flex flex-col-reverse md:flex-row items-center justify-center my-8">
-					<div className="md:w-1/3 p-8">
-						<h2 className="text-white text-3xl font-bold mb-4">Intro</h2>
+				<div className="flex flex-col md:flex-row items-center justify-center text-right my-8 md:px-8">
+					<div className="md:w-1/3 pt-8 md:pt-0 px-2 ">
+						<h3 className="text-white text-3xl font-bold mb-4">
+							Climate Change and Sustainability
+						</h3>{" "}
 						<p className="text-gray-200">
-							Your brief description goes here. Tell visitors about yourself,
-							your skills, and what you're passionate about.
+							Hiking at Peto Lake with a group of friends and enjoying the peace
+							that nature has to offer.
 						</p>
 					</div>
 					{/* Logo */}
-					<div className="md:w-2/3 p-8">
+					<div className="md:w-2/3 md:p-2">
 						<Image
 							src={CCnS}
-							alt="Aadityaa's Photo"
+							alt="Four people standing in front of a scenic view of mountains and lakes"
 							className="w-full h-auto rounded-lg shadow-md"
 						/>
 					</div>
 				</div>
 
 				{/* Section 3 */}
-				<div className="flex flex-col md:flex-row items-center justify-center my-8">
+				<div className="flex flex-col md:flex-row items-center justify-center my-8 md:px-8">
 					{/* Logo */}
-					<div className="md:w-2/3 p-8">
+					<div className="md:w-2/3 md:p-2">
 						<Image
 							src={Scuba}
-							alt="Aadityaa's Photo"
+							alt="Two people scuba diving"
 							className="w-full h-auto rounded-lg shadow-md"
 						/>
 					</div>
-					<div className="md:w-1/3 p-8">
-						<h2 className="text-white text-3xl font-bold mb-4">Intro</h2>
+					<div className="md:w-1/3 pt-8 md:pt-0 px-2">
+						<h3 className="text-white text-3xl font-bold mb-4">Travel</h3>{" "}
 						<p className="text-gray-200">
-							Your brief description goes here. Tell visitors about yourself,
-							your skills, and what you're passionate about.
+							Scuba diving at the Great Barrier Reef to witness the majestic
+							coral before they disappear.
 						</p>
 					</div>
 				</div>
